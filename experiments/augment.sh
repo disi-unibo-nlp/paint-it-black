3cfecd841c6590192f2d0ca2a844978b1e00cd11
dfreddi <d.freddi@unibo.it>
2026-04-10 13:01:25 +0200

    feat(dataprep): add low noise mix preset and tune low noise configs
    Signed-off-by: dfreddi <d.freddi@unibo.it>

--- experiments/augment.sh
+++ experiments/augment.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# Run the PDF augmentation pipeline.
-#
-# Usage:
-#   ./scripts/run_cont.sh bash experiments/augment.sh
-#   ./scripts/run_cont.sh bash experiments/augment.sh --num_augmentations 3
 
 for config in config/dataprep/augment_examples/*; do
     [ -f "$config" ] || continue
-    python3 src/dataprep/augment_pdfs.py --num_augmentations 8 --config "$config" --flat "$@"
+    python3 src/dataprep/augment_pdfs.py --num_augmentations 8 --config "$config" --flat
 done
 
 for config in config/dataprep/low_noise/*; do
     [ -f "$config" ] || continue
-    python3 src/dataprep/augment_pdfs.py --num_augmentations 8 --config "$config" --flat "$@"
+    python3 src/dataprep/augment_pdfs.py --num_augmentations 8 --config "$config" --flat
 done
+
+python3 src/dataprep/augment_pdfs.py --num_augmentations 10 --config config/dataprep/low_noise_mix.yaml --flat
+
+python3 src/dataprep/augment_pdfs.py \
+    --num_augmentations 1 \
+    --config config/dataprep/low_noise_mix.yaml \
+    --input_dir ./data/dev_docs \
+    --output_dir ./output/dev_docs_low_noise
