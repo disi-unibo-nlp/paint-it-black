7425390439d3dd6b1ef6cd2b1b81b4735c579875
dfreddi <d.freddi@unibo.it>
2026-05-18 13:32:02 +0000

    added ibm inference
    Signed-off-by: dfreddi <d.freddi@unibo.it>

--- experiments/run_inference.sh
+++ experiments/run_inference.sh
@@ -7,23 +7,28 @@
 #   ./experiments/run_inference.sh
 
 DATASET_DIR=disi-unibo-nlp/paint-it-black
-MODEL=Qwen/Qwen3.5-9B
+MODELS=(
+    Qwen/Qwen3.5-9B
+    # ...
+    )
 SPLIT=base
 RUN_NAME=e1_q35-9B_base
 
 # Run evaluations for base, medium, and hard splits
-for SPLIT in base; do # medium hard
-    RUN_NAME=e1_q35-9B_${SPLIT}
-    CONFIG=config/inference/base.yaml
 
-    echo "Running inference for split: $SPLIT"
-    ./scripts/run_vllm_inference.sh \
-        --model $MODEL \
-        --vllm_max_model_len 24576 \
-        --config $CONFIG \
-        --input_dataset $DATASET_DIR \
-        --input_split $SPLIT \
-        --from_hub \
-        --run_name $RUN_NAME
+for MODEL in "${MODELS[@]}"; do
+    for SPLIT in base medium hard; do
+        RUN_NAME=e1_q35-9B_${SPLIT}
+        CONFIG=config/inference/base.yaml
+        echo "Running inference for split: $SPLIT"
+        ./scripts/run_vllm_inference.sh \
+            --model $MODEL \
+            --vllm_max_model_len 24576 \
+            --config $CONFIG \
+            --input_dataset $DATASET_DIR \
+            --input_split $SPLIT \
+            --from_hub \
+            --run_name $RUN_NAME
+    done
 done
 
