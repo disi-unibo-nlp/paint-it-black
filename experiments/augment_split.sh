3312334b358caa9dc55b1ca90f0251fc4b1dfa01
dfreddi <d.freddi@unibo.it>
2026-05-27 07:29:44 +0000

    chore: restored commented commands
    Signed-off-by: dfreddi <d.freddi@unibo.it>

--- experiments/augment_split.sh
+++ experiments/augment_split.sh
@@ -13,29 +13,29 @@ SEED=1
     --config $CONFIG \
     --seed $SEED
 
-# OUTPUT_SPLIT=hard
-# CONFIG=config/dataprep/high_noise_mix.yaml
-# SEED=1
+OUTPUT_SPLIT=hard
+CONFIG=config/dataprep/high_noise_mix.yaml
+SEED=1
 
-# ./scripts/run_cont.sh python3 src/dataprep/augment_pdfs.py \
-#     --input_dataset $INPUT_DATASET \
-#     --input_split $INPUT_SPLIT \
-#     --output_split $OUTPUT_SPLIT \
-#     --config $CONFIG \
-#     --seed $SEED
+./scripts/run_cont.sh python3 src/dataprep/augment_pdfs.py \
+    --input_dataset $INPUT_DATASET \
+    --input_split $INPUT_SPLIT \
+    --output_split $OUTPUT_SPLIT \
+    --config $CONFIG \
+    --seed $SEED
 
-# resample hard
-# INPUT_DATASET=./data/paint_it_black
-# INPUT_SPLIT=base
-# OUTPUT_SPLIT=hard
-# CONFIG=config/dataprep/high_noise_mix.yaml
-# SEED=2
+resample hard
+INPUT_DATASET=./data/paint_it_black
+INPUT_SPLIT=base
+OUTPUT_SPLIT=hard
+CONFIG=config/dataprep/high_noise_mix.yaml
+SEED=2
 
-# ./scripts/run_cont.sh python3 src/dataprep/augment_pdfs.py \
-#     --input_dataset $INPUT_DATASET \
-#     --input_split $INPUT_SPLIT \
-#     --output_dataset $INPUT_DATASET \
-#     --output_split $OUTPUT_SPLIT \
-#     --config $CONFIG \
-#     --seed $SEED \
-#     --resample_ids 5 13 38 42 46 47 56 58 65 77 78 100 109 116 120 125 135 136
+./scripts/run_cont.sh python3 src/dataprep/augment_pdfs.py \
+    --input_dataset $INPUT_DATASET \
+    --input_split $INPUT_SPLIT \
+    --output_dataset $INPUT_DATASET \
+    --output_split $OUTPUT_SPLIT \
+    --config $CONFIG \
+    --seed $SEED \
+    --resample_ids 5 13 38 42 46 47 56 58 65 77 78 100 109 116 120 125 135 136
